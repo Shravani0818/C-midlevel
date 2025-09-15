23b8c7b7f05d8d19e6d1efd9cb60304c42365323
Shravani0818 <shravaniwaghmare2006@gmail.com>
2025-09-15 05:56:23 -0700

    Add files via upload

---
+++ smallest_of_two_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+ {
+    int number1, number2;
+
+    printf("Enter first number : ");
+    scanf("%d",&number1);
+    printf("Enter second number : ");
+    scanf("%d",&number2);
+    if (number1 < number2)
+     {
+        printf("%d is the smallest number ", number1);
+    } 
+    else if (number2 < number1) 
+    {
+        printf("%d is the smallest number ", number2);
+    } 
+    else
+     {
+        printf("Both numbers are equal");
+    }
+    return 0;
+}
