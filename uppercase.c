002514ce1841d3c59ffa27f170d1ba6ed7a304a4
Shravani0818 <shravaniwaghmare2006@gmail.com>
2025-09-29 08:39:21 -0700

    Add files via upload

---
+++ uppercase.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z') 
+    {
+        printf("%c is an uppercase letter.\n", ch);
+    } else 
+    {
+        printf("%c is NOT an uppercase letter.\n", ch);
+    }
+
+    return 0;
+}
