294c0a237c8985df8ddbea0644ddffbd7ac8ea8a
Shravani0818 <shravaniwaghmare2006@gmail.com>
2025-10-27 08:54:33 -0700

    Add files via upload

---
+++ num_divisible_by_7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number ; ");
+    scanf("%d", &n);
+    if (n % 7 == 0)
+        printf("Divisible by 7");
+    else
+        printf("Not divisible by 7");
+    return 0;
+}
