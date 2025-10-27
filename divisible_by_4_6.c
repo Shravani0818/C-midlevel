13c69bdfdcc3b1905eb1783e617171bcc62dfed7
Shravani0818 <shravaniwaghmare2006@gmail.com>
2025-10-27 09:21:47 -0700

    Add files via upload

---
+++ divisible_by_4_6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number : ");
+
+    scanf("%d", &n);
+    if (n % 4 == 0 && n % 6 == 0)
+        printf("Divisible by 4 and 6");
+    else
+        printf("Not divisible by both");
+    return 0;
+}
