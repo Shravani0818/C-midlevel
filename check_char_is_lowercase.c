c785497bb9c5fc033450d3033462d0acb96899c6
Shravani0818 <shravaniwaghmare2006@gmail.com>
2025-10-31 21:31:45 +0530

    Add program to check if character is lowercase

---
+++ check_char_is_lowercase.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch >= 'a' && ch <= 'z') {
+        printf("The character '%c' is a lowercase letter.\n", ch);
+    } else {
+        printf("The character '%c' is NOT a lowercase letter.\n", ch);
+    }
+
+    return 0;
+}
